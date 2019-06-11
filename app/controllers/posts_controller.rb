c877f4b306ac180d9c28218cd20d56835e7dc4f6
Robert Pedersen <rob@worldcrimeleague.org>
2019-06-11 12:23:35 -0400

    done for real

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,15 +12,16 @@ class PostsController < ApplicationController
 	end
 
 	def create
-	  @post = Post.new(params)
-	  @post.save
+
+		@post = Post.create(params.require(:post).permit(:title, :description))
 	  redirect_to post_path(@post)
 	end
 
 	def update
 	  @post = Post.find(params[:id])
-	  @post.update(params.require(:post))
-	  redirect_to post_path(@post)
+		@post = Post.update(params.require(:post).permit(:title, :description))
+		redirect_to post_path(@post)
+
 	end
 
 	def edit
